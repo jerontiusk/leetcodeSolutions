3411b0804f1de0414c45a962689a7429024f9c5e
Jerontius Jeyakumar <jjeyakumar@sisn.com>
2025-10-01 16:42:39 +0200

    leet code problem

---
+++ leetcode/Waterbottles.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.Design;
+
+
+new Solution().NumWaterBottles(15,4);
+public class Solution
+{
+    public int NumWaterBottles(int numBottles, int numExchange)
+    {
+        int total = numBottles;       
+        while (numBottles >= numExchange)
+        {
+            var exchangeBottles = numBottles / numExchange;
+            var remainingEmptyBottles = numBottles % numExchange;
+            total += exchangeBottles;
+            numBottles = exchangeBottles + remainingEmptyBottles;
+        }
+        return total;
+    }
+}
+
+
+
+
