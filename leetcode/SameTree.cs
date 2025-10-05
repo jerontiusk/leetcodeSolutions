521de77f9f77f751e28bb0cecd04158f51027a1f
Jeron Kumar <priya@sharuntech.net>
2025-10-05 19:30:13 +0200

    same tree changes

---
+++ leetcode/SameTree.cs
@@ -0,0 +1,37 @@
+﻿
+public class TreeNode
+{
+      public int val;
+      public TreeNode left;
+      public TreeNode right;
+     public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+     }
+ }
+
+public partial class Solution
+{
+    public bool IsSameTree(TreeNode p, TreeNode q)
+    {
+        Queue<(TreeNode,TreeNode)> queue = new();
+        queue.Enqueue((p, q));
+        while (queue.Count > 0)
+        {
+            (p, q) = queue.Dequeue();
+            if (p == null && q == null)
+                continue;
+            if (p == null || q == null)
+                return false;
+            if(p.val!=q.val) return false;
+            queue.Enqueue((p.left, q.left));
+            queue.Enqueue((p.right, q.right));
+
+
+        }
+        return true;
+
+    }
+}
--- leetcode/Waterbottles.cs
+++ leetcode/Waterbottles.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.ComponentModel.Design;
 
+new Solution().IsPalindrome("a");
 
 public partial class Solution
 {
