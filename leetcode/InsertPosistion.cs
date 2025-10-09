d91cd8e89c4a8c299073c2f36a5bb2e9d4975822
Jerontius Jeyakumar <jjeyakumar@sisn.com>
2025-10-09 18:18:23 +0200

    binary search

---
+++ leetcode/InsertPosistion.cs
@@ -0,0 +1,19 @@
+﻿public partial class Solution
+{
+    public int SearchInsert(int[] nums, int target)
+    {
+        int pivot, left = 0, right = nums.Length - 1;
+        while (left <= right)
+        {
+            pivot = left + (right - left) / 2;
+            if (nums[pivot] == target)
+                return pivot;
+            if (target < nums[pivot])
+                right = pivot - 1;
+            else
+                left = pivot + 1;
+        }
+
+        return left;
+    }
+}
