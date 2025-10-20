06e9453218faab2445770a980cce655616d6b522
Jerontius Jeyakumar <jjeyakumar@sisn.com>
2025-10-20 18:24:00 +0200

    operator changes

---
+++ leetcode/Operations.cs
@@ -0,0 +1,32 @@
+﻿public partial class Solution
+{
+    public int FinalValueAfterOperations(string[] operations)
+    {
+        int value = 0;
+       foreach (string operation in operations)
+        {
+            if (GetSummaryValue(operation) > 0)
+            {
+                value++;
+            }
+            else value--;
+        }
+        return value;
+    }
+    public int GetSummaryValue(string operation)
+    {
+        switch (operation)
+        {
+            case "--X":
+                return -1;
+            case "X++":
+                return 1;
+            case "++X":
+                return 1;
+            case "X--":
+                return -1;
+            default:
+                return 0;
+        }
+    }
+}
