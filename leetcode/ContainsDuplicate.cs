aa2f137ac758f8a45732545adb10b352225cb80a
Jeron Kumar <priya@sharuntech.net>
2025-11-01 13:46:50 +0100

    sliding window

---
+++ leetcode/ContainsDuplicate.cs
@@ -0,0 +1,22 @@
+﻿public partial class Solution
+{
+    public bool ContainsNearbyDuplicate(int[] nums, int k)
+    {
+        var keyHashSet = new HashSet<int>();
+        for (int x = 0; x < nums.Length; x++)
+        {
+            if (keyHashSet.Contains(nums[x])) return true;
+            else
+            {
+                keyHashSet.Add(nums[x]);
+                if (keyHashSet.Count > k)
+                {
+
+                    keyHashSet.Remove(nums[x - k]);
+                }
+            }
+        }
+        return false;
+    }
+
+}
