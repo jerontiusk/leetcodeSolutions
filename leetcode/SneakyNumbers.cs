74611907262d1494f41cfaab8b6b74e1ab2849ac
Jerontius Jeyakumar <jjeyakumar@sisn.com>
2025-11-17 19:00:05 +0100

    commint

---
+++ leetcode/SneakyNumbers.cs
@@ -0,0 +1,20 @@
+﻿public partial class Solution
+{
+    public int[] GetSneakyNumbers(int[] nums)
+    {
+        var result = new List<int>();
+        var map = new HashSet<int>();
+        foreach (int i in nums)
+        {
+            if(!map.TryGetValue(i,out int c))
+            {
+                map.Add(i);
+            }
+            else
+            {
+                result.Add(i);
+            }
+        }
+        return result.ToArray();
+    }
+}
