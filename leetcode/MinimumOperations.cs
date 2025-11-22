77578c6c5cce225061973d6b26d3e75b6a2f3d7f
Jeron Kumar <priya@sharuntech.net>
2025-11-22 21:38:42 +0100

    operations

---
+++ leetcode/MinimumOperations.cs
@@ -0,0 +1,14 @@
+﻿public partial class Solution
+{
+    public int MinimumOperations(int[] nums)
+    {
+        int operationsCount = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            var number = nums[i] ;
+            operationsCount += Math.Min(number % 3, 3 - number % 3);
+
+        }
+        return operationsCount;
+    }
+}
