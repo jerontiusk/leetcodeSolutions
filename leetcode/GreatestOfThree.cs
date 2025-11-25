6370b0b65286036a10b2438f553b519b238102ef
Jeron Kumar <priya@sharuntech.net>
2025-11-25 22:05:12 +0100

    leat of three

---
+++ leetcode/GreatestOfThree.cs
@@ -0,0 +1,19 @@
+﻿public partial class Solution
+{
+    public int MaxSumDivThree(int[] nums)
+    {
+        int[] f = { 0, int.MinValue, int.MinValue };
+        foreach (int num in nums)
+        {
+            int[] g = new int[3];
+            Array.Copy(f, 0, g, 0, 3);
+            for (int i = 0; i < 3; ++i)
+            {
+                g[(i + num % 3) % 3] =
+                    Math.Max(g[(i + num % 3) % 3], f[i] + num);
+            }
+            f = g;
+        }
+        return f[0];
+    }
+}
---
+++ leetcode/LeastOfThreecs.cs
@@ -0,0 +1,17 @@
+﻿public partial class Solution
+{
+    public int SmallestRepunitDivByK(int k)
+    {
+        int remainder = 0;
+        for (int length_N = 1; length_N <= k; length_N++)
+        {
+            remainder = (remainder * 10 + 1) % k;
+            if (remainder == 0)
+            {
+                return length_N;
+            }
+        }
+        return -1;
+
+    }
+}
