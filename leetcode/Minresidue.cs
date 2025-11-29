5a637d129a452effaa924478d6c4e144b1ef554e
Jeron Kumar <priya@sharuntech.net>
2025-11-29 19:38:57 +0100

    min count

---
+++ leetcode/Minresidue.cs
@@ -0,0 +1,11 @@
+﻿namespace leetcode
+{
+    public partial class Solution
+    {
+        public int MinOperations(int[] nums, int k)
+        {
+            var sum = nums.Sum(x => x);
+            return sum % k;
+        }
+    }
+}
